e4bd0bd3fab5812da6bbb87b8145fe7c11db00b2
Ahmed Mohamed <ahmedabozied699@gmail.com>
2023-04-08 23:51:20 +0200

    add Contact us Section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import AboutUs from './screens/AboutUs'
 import HomeScreen from './screens/HomeScreen'
 import NotFound from './screens/NotFound'
+import ContactUs from './screens/ContactUs'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomeScreen/>}/>
       <Route path='/about-us' element={<AboutUs/>}/>
+      <Route path='/contact-us' element={<ContactUs/>}/>
       <Route path='*' element={<NotFound/>}/>
     </Routes>
 
--- src/screens/ContactUs.js
+++ src/screens/ContactUs.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import Layout from "../layout/Layout";
+import Head from "../components/Head";
+import { FiMail, FiPhoneCall,FiMapPin } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const ContactUs = () => {
-  return (
-    <div>ContactUs</div>
-  )
-}
+  const contactData = [
+    {
+      id: 1,
+      title: "Email us",
+      info: " Interactively grow backend ideas for cross-platform models.",
+      icon: FiMail,
+      contact: "ahmed123@gmail.com",
+    },
+    {
+      id: 2,
+      title: "Call us",
+      info: " Distinctively exploit optimal alignments for intuitive bandwidth.",
+      icon: FiPhoneCall,
+      contact: "+201094072207",
+    },
+    {
+      id: 3,
+      title: "Location ",
+      icon: FiMapPin,
 
-export default ContactUs
+      info: "  Egypt , Mansoura,",
+      contact:""
+    },
+  ];
+  return (
+    <Layout>
+      <div className="min-h-screen container mx-auto px-2 my-2">
+        <Head title="Contact Us" />
+        <div className="grid md:grid-cols-2 gap-6 lg:mt-20 mt-10 lg:grid-cols-3 xl:gap-8 ">
+         {
+          contactData.map((item)=>(
+<div key={item.id} className="border border-border flex-colo p-10 bg-dry rounded-lg text-center ">
+  <span className="flex-colo  w-20 h-20 rounded-full bg-main text-submain text-2xl mb-4">
+    <item.icon/>
+  </span>
+  <h5 className="text-xl font-semibold mb-2">{item.title}</h5>
+  <p className="mb-0 text-sm text-text leading-7"> <Link className="text-blue-400">{item.contact}</Link>{item.info}</p>
+</div>
+          ))
+         }
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default ContactUs;
